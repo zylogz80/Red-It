94e526f051dfe6ad4af721ac859ae00c12afe39f
Adam Drew <adamrdrew@gmail.com>
2011-07-31 16:21:22 -0400

    Had to git add some files I forgot

---
+++ redit/source/errorPopup.js
@@ -0,0 +1,41 @@
+/*
+    Red It: A beautiful and functional Reddit browser for webOS tablets 
+    Copyright (C) 2011  Adam R. Drew adam@linkedlistcorruption.com
+
+	This file is part of Red It.
+
+    Red It is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+enyo.kind({
+
+	kind: enyo.Popup, 
+	name: "errorPopup", 
+	dismissWithClick: true, 
+	modal: true,
+	align: "center",
+	components: [
+		{kind: enyo.HFlexBox, align: "center", name: "theBox" , components: [	
+			{kind: enyo.Image, src: "icons/exclaim.png"},
+			{content: "Uh oh! I can't talk to Reddit! Is Reddit down?"}, 
+			{content: "Do you have no internet connection? Are you behind a firewall or proxy?"},
+			{content: "Sorry!:("},
+			{content: "Error Code: STUFFSBROKE"}
+		]}
+	],
+	
+	showError: function(errorText) {
+		this.$.test.setContent(errorText);
+	}
+	
+})
